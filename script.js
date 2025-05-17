0c1b4f00ec06fc751916b8bd58c3fb74e3f52a3b
prabakaran <prabhavicky027@gmail.com>
2025-05-16 16:00:27 -0800

    day3

---
+++ script.js
@@ -0,0 +1,22 @@
+var a=10
+a="theeran"
+console.log(a)
+let b="react"
+
+function test(c,b){
+    var a=c+b
+    console.log(a)
+}
+console.log(b)
+test(10,20)
+test(10,5) 
+  
+var arr=[10,20,30,40];
+console.log(arr)
+arr[3]=70
+
+function check(){
+    for(i=0;i<=5;i++)
+        console.log(a)   
+}
+check()
